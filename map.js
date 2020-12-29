2f71a34b31d90989a7f40c0403f237dca3eccda9
Designcat2020 <66333872+Designcat2020@users.noreply.github.com>
2020-12-28 22:06:06 -0500

    Add files via upload

---
+++ map.js
@@ -0,0 +1,133 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoieWFuc3VuMjAyMCIsImEiOiJjazg4dmFsbGcwMGcwM2xxc2Zla21zZG91In0.Kkqjs0MWxmSEeqe7yO-k5g';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/light-v10',
+center: [0, 20],
+zoom: 2
+});
+ 
+map.on('load', function () {
+map.addSource('activities', {
+type: 'geojson',
+data: 'data/data.geojson',
+cluster: true,
+clusterMaxZoom: 14, 
+clusterRadius: 50 
+});
+ 
+map.addLayer({
+id: 'clusters',
+type: 'circle',
+source: 'activities',
+filter: ['has', 'point_count'],
+paint: {
+'circle-color': ['step',['get', 'point_count'],
+'#51bbd6',5,
+'#f1f075',10,
+'#f28cb1'
+],
+'circle-radius': ['step',['get', 'point_count'],
+20,5,
+30,10,
+40]
+}
+});
+ 
+map.addLayer({
+id: 'cluster-count',
+type: 'symbol',
+source: 'activities',
+filter: ['has', 'point_count'],
+layout: {
+'text-field': '{point_count_abbreviated}',
+'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+'text-size': 12
+}
+});
+ 
+map.addLayer({
+id: 'unclustered-point',
+type: 'circle',
+source: 'activities',
+filter: ['!', ['has', 'point_count']],
+paint: {
+'circle-color': '#11b4da',
+'circle-radius': 4,
+'circle-stroke-width': 1,
+'circle-stroke-color': '#fff'
+}
+});
+ 
+map.on('click', 'clusters', function (e) {
+var features = map.queryRenderedFeatures(e.point, {
+layers: ['clusters']
+});
+var clusterId = features[0].properties.cluster_id;
+map.getSource('activities').getClusterExpansionZoom(
+clusterId,
+function (err, zoom) {
+if (err) return;
+ 
+map.easeTo({
+center: features[0].geometry.coordinates,
+zoom: zoom
+});
+}
+);
+});
+ 
+map.on('click', 'unclustered-point', function (e) {
+var coordinates = e.features[0].geometry.coordinates.slice();
+var Loc = e.features[0].properties.Loc;
+var Act = e.features[0].properties.Act;
+
+
+while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+}
+ 
+new mapboxgl.Popup()
+.setLngLat(coordinates)
+.setHTML(
+'Location:' + Loc + '<br>Activities: ' + Act
+)
+.addTo(map);
+});
+ 
+map.on('mouseenter', 'clusters', function () {
+map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'clusters', function () {
+map.getCanvas().style.cursor = '';
+});
+});
+
+var toggleableLayerIds = ['Produce in NYC By SunYan'];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function(e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
